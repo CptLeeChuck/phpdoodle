ed030ce52d41395ac8e3f112101af01401678379
Cpt LeeChuck <f@xn.cx>
2020-01-26 21:58:41 +0100

    cleaning in update file

--- update_via_ajax.php
+++ update_via_ajax.php
@@ -2,12 +2,10 @@
 	if($_POST['id'])
 	{
 		include("db.php");
-		$id=mysqli_real_escape_string($link, $_POST['id']);
-		//$name=mysqli_real_escape_string($link, $_POST['name']);
-		//$wurst=mysqli_real_escape_string($link, $_POST['wurst']);
-		//$getraenk=mysqli_real_escape_string($link, $_POST['getraenk']);
-		
-		//$sql = "update test set name='$name',getraenk='$getraenk',wurst='$wurst' where id='$id'";
+		$id = mysqli_real_escape_string($link, $_POST['id']);
+
+		// Loop all columns and create the UPDATE sql staement like this example:
+		// UPDATE TABLE SET firstcolumn='$firstcolumn', secndcolumn='$secndcolumn' WHERE id='$id'";
 		
 		$sql = "UPDATE $mysql_table SET ";
 		$firstloop = true;
