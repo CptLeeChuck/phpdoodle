3c105b08768e17efb521e6ee07838b714344d373
Cpt LeeChuck <f@xn.cx>
2020-01-26 21:53:19 +0100

    JQuery update 1.5 to 3.4.1

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <html class="no-js" lang="en">
 
 <head>
-	<script type="text/javascript" src="jq15.js"></script> <!-- JQuery 1.5 :-o -->
+	<script type="text/javascript" src="jquery-3.4.1.min.js"></script>
 	<link rel="stylesheet" href="./bootstrap-4.4.1/css/bootstrap.min.css">
 	<script type="text/javascript" src="./bootstrap-4.4.1/js/bootstrap.min.js"></script>
 	<script type="text/javascript">
